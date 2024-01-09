e3f56ed3eb8dbf3049b1933a0f1b83bf2782600e
Anderson Mendes <developer.andersonmendes@gmail.com>
2024-01-08 21:07:43 -0300

    Aula 333 - Finalizada

--- lib/screens/place_form_screen.dart
+++ lib/screens/place_form_screen.dart
@@ -1,3 +1,4 @@
+import 'package:curso_flutter_places/widgets/image_input.dart';
 import 'package:flutter/material.dart';
 
 class PlaceFormScreen extends StatefulWidget {
@@ -8,14 +9,56 @@ class PlaceFormScreen extends StatefulWidget {
 }
 
 class _PlaceFormScreenState extends State<PlaceFormScreen> {
+  final _titleController = TextEditingController();
+
+  void _submitForm() {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Novo Lugar'),
       ),
-      body: const Center(
-        child: Text('Form!'),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  children: [
+                    TextField(
+                      controller: _titleController,
+                      decoration: InputDecoration(
+                        labelText: 'Título',
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    ImageInput(),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          ElevatedButton.icon(
+            onPressed: _submitForm,
+            icon: Icon(
+              Icons.add,
+              color: Colors.white,
+            ),
+            label: Text(
+              'Adicionar',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all<Color>(
+                  Theme.of(context).colorScheme.primary),
+            ),
+          ),
+        ],
       ),
     );
   }
