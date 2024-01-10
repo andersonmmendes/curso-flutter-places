a31c091c646c9bb07080dbd3100462137fabca9a
Anderson Mendes <developer.andersonmendes@gmail.com>
2024-01-10 19:51:39 -0300

    Aula 338 - Finalizada

--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -1,5 +1,7 @@
+import 'package:curso_flutter_places/providers/great_places.dart';
 import 'package:curso_flutter_places/utils/app_routes.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class PlacesListScreen extends StatelessWidget {
   const PlacesListScreen({super.key});
@@ -11,15 +13,30 @@ class PlacesListScreen extends StatelessWidget {
         title: const Text('Meus Lugares'),
         actions: [
           IconButton(
-            icon: Icon(Icons.add),
+            icon: const Icon(Icons.add),
             onPressed: () {
               Navigator.of(context).pushNamed(AppRoutes.placeForm);
             },
           ),
         ],
       ),
-      body: const Center(
-        child: CircularProgressIndicator(),
+      body: Consumer<GreatPlaces>(
+        builder: (context, greatPlaces, child) => greatPlaces.itemsCount == 0
+            ? child!
+            : ListView.builder(
+                itemCount: greatPlaces.itemsCount,
+                itemBuilder: (context, index) => ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage:
+                        FileImage(greatPlaces.itemByIndex(index).image),
+                  ),
+                  title: Text(greatPlaces.itemByIndex(index).title),
+                  onTap: () {},
+                ),
+              ),
+        child: const Center(
+          child: Text('Não há lugares ainda'),
+        ),
       ),
     );
   }
